7e8ebe78b3835e49056d3a0bf54f8d5dc341dea9
Yen-Hsun, Huang <s011208@gmail.com>
2014-03-12 23:51:59 +0800

    add library

--- U2bPlayer/src/com/yenhsun/u2bplayer/MainActivity.java
+++ U2bPlayer/src/com/yenhsun/u2bplayer/MainActivity.java
@@ -1,23 +1,42 @@
 
 package com.yenhsun.u2bplayer;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStyle;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(U2bApplication.DEVELOPER_KEY, this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onInitializationFailure(Provider arg0, YouTubeInitializationResult arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        // TODO Auto-generated method stub
+        if (!wasRestored) {
+            player.setPlayerStyle(PlayerStyle.MINIMAL);
+            player.loadVideo("DDs5bXh4erM");
+        }
+
     }
 
 }
---
+++ U2bPlayer/src/com/yenhsun/u2bplayer/U2bApplication.java
@@ -0,0 +1,8 @@
+
+package com.yenhsun.u2bplayer;
+
+import android.app.Application;
+
+public class U2bApplication extends Application {
+    public static final String DEVELOPER_KEY = "AIzaSyAI7xUOv1W_jKNR4XjDIGaqKK22nt74d50";
+}
