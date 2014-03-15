c51b1897513d22c9c72de8f40649656595480955
Yen-Hsun, Huang <s011208@gmail.com>
2014-03-15 19:32:32 +0800

    keep new

--- U2bPlayer/src/com/yenhsun/u2bplayer/MainActivity.java
+++ U2bPlayer/src/com/yenhsun/u2bplayer/MainActivity.java
@@ -1,64 +1,44 @@
 
 package com.yenhsun.u2bplayer;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStyle;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.app.Activity;
 
-public class MainActivity extends Activity {
-
-    TextView mResult;
+public class MainActivity extends YouTubeBaseActivity implements
+        YoutubeIdParser.YoutubeIdParserResultCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mResult = (TextView) findViewById(R.id.parse_result);
-
+        YoutubeIdParser.showYoutubeResult(new String[] {
+                "五月天", "入陣曲"
+        }, this);
     }
 
-    private void showYoutubeResult() {
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                String result = "";
-                JSONArray jArray = YoutubeIdParser
-                        .parse("https://gdata.youtube.com/feeds/api/videos?q="
-                                + Uri.encode("五月天+入陣曲")
-                                + "&max-results=5&alt=json");
-                if (jArray != null) {
-                    try {
-                        for (int i = 0; i < jArray.length(); i++) {
-                            String raw = ((JSONObject) jArray.get(i)).getJSONObject("id")
-                                    .getString("$t");
-                            result += raw.substring(raw.lastIndexOf("/") + 1) + "\n";
-                        }
-                    } catch (JSONException e) {
-                        // TODO Auto-generated catch block
-                        Log.e("QQQQ", "failed", e);
-                    }
-
-                    final String r = result;
-                    runOnUiThread(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            mResult.setText(r);
-                            Log.e("QQQQ", "DONE");
-                        }
-                    });
-                } else
-                    Log.e("QQQQ", "null");
-
-            }
-        }).start();
+    @Override
+    public void setResult(ArrayList<String> idList) {
+        for (String id : idList) {
+            Log.e("QQQQ", id);
+        }
     }
 
 }
--- U2bPlayer/src/com/yenhsun/u2bplayer/YoutubeIdParser.java
+++ U2bPlayer/src/com/yenhsun/u2bplayer/YoutubeIdParser.java
@@ -7,17 +7,60 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.net.Uri;
 import android.util.Log;
 
 public class YoutubeIdParser {
     // https://developers.google.com/youtube/2.0/developers_guide_protocol_api_query_parameters
     private static final String TAG = "YoutubeIdParser";
+
+    public interface YoutubeIdParserResultCallback {
+        public void setResult(ArrayList<String> idList);
+    }
+
+    public static void showYoutubeResult(final String[] searchKey,
+            final YoutubeIdParserResultCallback callback) {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                final ArrayList<String> playList = new ArrayList<String>();
+                String key = "";
+                for (String k : searchKey) {
+                    key += k + "+";
+                }
+                if ("".equals(key))
+                    return;
+                else
+                    key = key.substring(0, key.length() - 1);
+                Log.e("QQQQ", "key: " + key);
+                JSONArray jArray = YoutubeIdParser
+                        .parse("https://gdata.youtube.com/feeds/api/videos?q="
+                                + Uri.encode(key)
+                                + "&max-results=5&alt=json");
+                if (jArray != null) {
+                    try {
+                        for (int i = 0; i < jArray.length(); i++) {
+                            String raw = ((JSONObject) jArray.get(i)).getJSONObject("id")
+                                    .getString("$t");
+                            playList.add(raw.substring(raw.lastIndexOf("/") + 1));
+                        }
+                        if (callback != null) {
+                            callback.setResult(playList);
+                        }
+                    } catch (JSONException e) {
+                    }
+                }
+            }
+        }).start();
+    }
 
     public static JSONArray parse(String url) {
         return convertFromStringToJson(parseOnInternet(url));
