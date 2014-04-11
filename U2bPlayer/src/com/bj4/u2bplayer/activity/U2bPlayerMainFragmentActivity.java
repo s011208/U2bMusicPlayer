f8703d493998f4550176b7ed72107f1512fdb567
Yen-Hsun, Huang <s011208@gmail.com>
2014-04-12 00:07:49 +0800

    using fragmentActivity instead

--- U2bPlayer/src/com/bj4/u2bplayer/activity/U2bPlayerMainFragmentActivity.java
+++ U2bPlayer/src/com/bj4/u2bplayer/activity/U2bPlayerMainFragmentActivity.java
@@ -1,30 +1,72 @@
 
 package com.bj4.u2bplayer.activity;
 
+import java.lang.reflect.Method;
+
 import com.bj4.u2bplayer.PlayMusicApplication;
 import com.bj4.u2bplayer.R;
 import com.bj4.u2bplayer.dialogs.MainActivityOptionDialog;
 
-import android.app.Activity;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
-public class U2bPlayerBaseActivity extends Activity {
-
+public class U2bPlayerMainFragmentActivity extends FragmentActivity {
     private static final String TAG = "QQQQ";
 
     private static final boolean DEBUG = true && PlayMusicApplication.OVERALL_DEBUG;
 
+    private RelativeLayout mMainLayout;
+
     private ImageButton mOptionBtn;
 
-    public void onResume() {
-        super.onResume();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.u2b_main_activity);
+        initMainLayout();
+        initComponents();
         initActionBarComponents();
+    }
+
+    /**
+     * do not edit this method
+     */
+    private void initMainLayout() {
+        boolean hasNavigationBar = true;
+        try {
+            Class<?> c = Class.forName("android.view.WindowManagerGlobal");
+            Method m = c.getDeclaredMethod("getWindowManagerService", new Class<?>[] {});
+            Object windowManagerService = m.invoke(null, new Object[] {});
+            c = windowManagerService.getClass();
+            m = c.getDeclaredMethod("hasNavigationBar", new Class<?>[] {});
+            hasNavigationBar = (Boolean)m.invoke(windowManagerService, new Object[] {});
+            if (DEBUG)
+                Log.d(TAG, "hasNavigationBar: " + hasNavigationBar);
+        } catch (Exception e) {
+            if (DEBUG)
+                Log.w(TAG, "failed to get windowManagerService", e);
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // TODO do something about transparent navigation bar
+            int statusBarHeight = (int)getResources().getDimension(R.dimen.status_bar_height);
+            int navigationBarHeight = hasNavigationBar ? (int)getResources().getDimension(
+                    R.dimen.navigation_bar_height) : 0;
+            mMainLayout = (RelativeLayout)findViewById(R.id.u2b_main_activity_main_layout);
+            mMainLayout.setPadding(mMainLayout.getPaddingLeft(), statusBarHeight,
+                    mMainLayout.getPaddingRight(), navigationBarHeight);
+        }
+    }
+
+    private void initComponents() {
     }
 
     public void initActionBarComponents() {
@@ -51,8 +93,8 @@ public class U2bPlayerBaseActivity extends Activity {
                     int[] location = {
                             size.x - location1[0] - r.width(), 2 * location1[1]
                     };
-                    new MainActivityOptionDialog(U2bPlayerBaseActivity.this, mOptionCallback,
-                            location).show(getFragmentManager(), "");
+                    new MainActivityOptionDialog(U2bPlayerMainFragmentActivity.this,
+                            mOptionCallback, location).show(getFragmentManager(), "");
                 }
             });
         }
