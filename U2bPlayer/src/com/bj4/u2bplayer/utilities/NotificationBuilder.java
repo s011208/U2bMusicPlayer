e9eb301b6df94fdca08d5c3caf8e635b137b0d1d
citsituamai <citsituamai@gmail.com>
2014-05-08 04:33:28 +0800

    revise Notification

--- U2bPlayer/src/com/bj4/u2bplayer/utilities/NotificationBuilder.java
+++ U2bPlayer/src/com/bj4/u2bplayer/utilities/NotificationBuilder.java
@@ -1,17 +1,22 @@
 
 package com.bj4.u2bplayer.utilities;
 
-import com.bj4.u2bplayer.R;
-import com.bj4.u2bplayer.activity.U2bPlayerMainFragmentActivity;
+import java.io.IOException;
 
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.bj4.u2bplayer.R;
+import com.bj4.u2bplayer.activity.U2bPlayerMainFragmentActivity;
 
 public class NotificationBuilder {
     private static final String TAG = "NotificationBuilder";
@@ -25,42 +30,43 @@ public class NotificationBuilder {
     private NotificationBuilder() {
     }
 
-    public static Notification createSimpleNotification(final Context context,
-            final PlayListInfo info) {
-        Intent notifyIntent = new Intent(context, U2bPlayerMainFragmentActivity.class);
-        PendingIntent appIntent = PendingIntent.getActivity(context, 0, notifyIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        String title = info == null ? "U2B notification" : info.mMusicTitle;
-        String content = info == null ? "no music information" : info.mAlbumTitle + "  "
-                + info.mArtist;
-        try {
-            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                Notification.Builder builder = new Notification.Builder(context);
-                builder.setContentTitle(title).setContentText(content)
-                        .setSmallIcon(R.drawable.ic_launcher).setContentIntent(appIntent)
-                        .setAutoCancel(false).setOngoing(true);
-                int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-                if (currentapiVersion >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-                    builder.setPriority(Notification.PRIORITY_LOW);
-                    return builder.build();
-                } else {
-                    return builder.getNotification();
-                }
-            } else {
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-                builder.setContentTitle(title).setContentText(content)
-                        .setSmallIcon(R.drawable.ic_launcher).setContentIntent(appIntent)
-                        .setPriority(Notification.PRIORITY_LOW).setAutoCancel(false)
-                        .setOngoing(true);
-                return builder.build();
-            }
-        } catch (Exception e) {
-            if (DEBUG)
-                Log.w(TAG, "failed to create notification", e);
-        }
-        return null;
-    }
+//    public static Notification createSimpleNotification(final Context context,
+//            final PlayListInfo info) {
+//        Intent notifyIntent = new Intent(context, U2bPlayerMainFragmentActivity.class);
+//        PendingIntent appIntent = PendingIntent.getActivity(context, 0, notifyIntent,
+//                PendingIntent.FLAG_UPDATE_CURRENT);
+//        String title = info == null ? "U2B notification" : info.mMusicTitle;
+//        String content = info == null ? "no music information" : info.mAlbumTitle + "  "
+//                + info.mArtist;
+//        try {
+//            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//                Notification.Builder builder = new Notification.Builder(context);
+//                builder.setContentTitle(title).setContentText(content)
+//                        .setSmallIcon(R.drawable.ic_launcher).setContentIntent(appIntent)
+//                        .setAutoCancel(false).setOngoing(true);
+//                int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+//                if (currentapiVersion >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+//                    builder.setPriority(Notification.PRIORITY_LOW);
+//                    return builder.build();
+//                } else {
+//                    return builder.getNotification();
+//                }
+//            } else {
+//                NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+//                builder.setContentTitle(title).setContentText(content)
+//                        .setSmallIcon(R.drawable.ic_launcher).setContentIntent(appIntent)
+//                        .setPriority(Notification.PRIORITY_LOW).setAutoCancel(false)
+//                        .setOngoing(true);
+//                return builder.build();
+//            }
+//        } catch (Exception e) {
+//            if (DEBUG)
+//                Log.w(TAG, "failed to create notification", e);
+//        }
+//        return null;
+//    }
 
+    
     public static void handleSimpleNotification(final Context context, final PlayListInfo info) {
         try {
             NotificationManager nm = (NotificationManager) context
@@ -101,6 +107,39 @@ public class NotificationBuilder {
         } catch (Exception e) {
             if (DEBUG)
                 Log.w(TAG, "failed to create notification", e);
+        }
+        return null;
+    }
+    
+    
+    public static Notification createSimpleNotification(final Context context, final PlayListInfo info) {
+        try {
+            String title = info == null ? "U2B notification" : info.mMusicTitle;
+            String content = info == null ? "no music information" : info.mAlbumTitle + "  " + info.mArtist;
+            Bitmap remote_picture = null;
+      
+            remote_picture = BitmapFactory.decodeResource(context.getResources(), R.drawable.kkbox);
+            NotificationCompat.BigPictureStyle notiStyle = new NotificationCompat.BigPictureStyle();
+            notiStyle.setBigContentTitle(title);
+            notiStyle.setSummaryText(content);
+            Intent resultIntent = new Intent(context, U2bPlayerMainFragmentActivity.class);
+
+            PendingIntent appIntent = PendingIntent.getActivity(context, 0, resultIntent, 0);
+            RemoteViews expandedView = new RemoteViews(context.getPackageName(), R.layout.notification_view);
+            expandedView.setTextViewText(R.id.text_view, title+"\n"+content);
+            expandedView.setOnClickPendingIntent(R.id.play_info_pause, appIntent); 
+
+            Notification notification = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setOngoing(true) 
+                    .setLargeIcon(remote_picture)
+                    .setContent(expandedView)
+                    .setStyle(notiStyle)
+                    .build();
+            
+            return notification;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
