abebe68fa41a598c9db8cf1a9c7ece48eac4430a
Yen-Hsun, Huang <s011208@gmail.com>
2014-05-09 14:20:27 +0800

    add widget click actions

--- U2bPlayer/src/com/bj4/u2bplayer/widget/SimplePlayWidget.java
+++ U2bPlayer/src/com/bj4/u2bplayer/widget/SimplePlayWidget.java
@@ -4,14 +4,26 @@ package com.bj4.u2bplayer.widget;
 import com.bj4.u2bplayer.R;
 import com.bj4.u2bplayer.utilities.PlayListInfo;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 public class SimplePlayWidget extends AppWidgetProvider {
+    private static final String PLAY = "play";
+
+    private static final String PAUSE = "pause";
+
+    private static final String NEXT = "next";
+
+    private static final String PREVIOUS = "previous";
+
+    private static final String FAVORITE = "favorite";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         performUpdate(context, null);
@@ -32,7 +44,32 @@ public class SimplePlayWidget extends AppWidgetProvider {
                 infoString = info.mArtist + ": " + info.mMusicTitle;
             }
             updateViews.setTextViewText(R.id.simple_play_widget_info, infoString);
+            updateViews.setOnClickPendingIntent(R.id.simple_play_widget_favorite,
+                    getPendingSelfIntent(context, FAVORITE));
+            updateViews.setOnClickPendingIntent(R.id.simple_play_widget_previous,
+                    getPendingSelfIntent(context, PREVIOUS));
+            updateViews.setOnClickPendingIntent(R.id.simple_play_widget_play,
+                    getPendingSelfIntent(context, PLAY));
+            updateViews.setOnClickPendingIntent(R.id.simple_play_widget_next,
+                    getPendingSelfIntent(context, NEXT));
             awm.updateAppWidget(appWidgetId, updateViews);
         }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        String action = intent.getAction();
+        if (PLAY.equals(action)) {
+        } else if (FAVORITE.equals(action)) {
+        } else if (NEXT.equals(action)) {
+        } else if (NEXT.equals(action)) {
+        }
+    }
+
+    protected static PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, SimplePlayWidget.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
